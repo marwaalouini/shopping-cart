d222bc1d78fe231a45f1ff5650bbf9885ecdc2b6
marwaalouini <marwaalouini123@gmail.com>
2023-11-13 20:41:19 +0100

    changement

--- main.js
+++ main.js
@@ -2,36 +2,30 @@ $(".heart.fa").click(function() {
     $(this).toggleClass("fa-heart fa-heart-o");
 });
 
-// select element from html
-
-const shoppingCard = document.getElementById("boxes");
-const products = document.getElementsByClassName("product");
+// Select elements from HTML
+const products = document.querySelectorAll(".product");
 const totalprice = document.getElementById("total-cost");
 
 let total = 0;
 
-for (let i = 0; i < products.length; i++) {
-    const product = products[i];
-    const minus = product.getElementsByClassName("minus")[0];
-    const plus = product.getElementsByClassName("plus")[0];
-
-    const quantityInput = product.getElementsByTagName("input")[0];
-    const remove = product.getElementsByClassName("btn btn-danger delete btn")[0];
-    const price = parseInt(
-        product.getElementsByClassName("price")[0].innerText.substring(1)
-    );
+products.forEach(product => {
+    const minus = product.querySelector(".minus");
+    const plus = product.querySelector(".plus");
+    const quantityInput = product.querySelector("input");
+    const remove = product.querySelector(".delete");
+    const price = parseInt(product.querySelector(".price").innerText);
 
     minus.addEventListener('click', () => {
         if (quantityInput.value > 1) {
             quantityInput.value--;
             total -= price;
-            totalprice.innerText = `Total Price : $${total} `;
+            totalprice.innerText = `Total Price : $${total.toFixed(2)}`;
         }
     });
 
     plus.addEventListener("click", () => {
         quantityInput.value++;
         total += price;
-        totalprice.innerText = `Total Price : $${total} `;
+        totalprice.innerText = `Total Price : $${total.toFixed(2)}`;
     });
-}
+});
